40a92b0cf555994b3098f4ab5fc4f18a68c2a289
CPS <holy.dandelion@139.com>
2023-03-23 22:44:55 +0800

    🐛修复(custom): 修复一些build错误

--- src/components/Iconfont/index.tsx
+++ src/components/Iconfont/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+
 import classnames from "classnames";
 import "./iconfont.css";
 
@@ -16,14 +18,19 @@ declare global {
 }
 
 export function IconfontInit(src: string = "") {
-  if (window.CPS_ICONFONT_INIT) return;
-
-  const ICONFIGT_SRC = src || "//at.alicdn.com/t/c/font_3959151_f86s1etjfv.js";
-  const scriptElem = document.createElement("script");
-
-  scriptElem.src = ICONFIGT_SRC;
-  document.body.appendChild(scriptElem);
-  window.CPS_ICONFONT_INIT = true;
+  return (
+    <BrowserOnly>
+      {() => {
+        if (window.CPS_ICONFONT_INIT) return;
+        const ICONFIGT_SRC = src || "//at.alicdn.com/t/c/font_3959151_f86s1etjfv.js";
+        const scriptElem = document.createElement("script");
+        scriptElem.src = ICONFIGT_SRC;
+        document.body.appendChild(scriptElem);
+        window.CPS_ICONFONT_INIT = true;
+        return <div className="hidden">{window.CPS_ICONFONT_INIT}</div>;
+      }}
+    </BrowserOnly>
+  );
 }
 
 export default function Iconfont({ iconName, className = "", iconPrefix = "", ...restProps }: IconfontProps) {
