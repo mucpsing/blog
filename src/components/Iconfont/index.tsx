9b6efee744ac6b67e9451dd36a72296832b8e21f
cpasion-office-win10 <373704015@qq.com>
2023-03-25 04:04:35 +0800

    🐛修复(custom):

--- src/components/Iconfont/index.tsx
+++ src/components/Iconfont/index.tsx
@@ -18,6 +18,7 @@ declare global {
 
 export function createIconfontScriptsToDOM(src: string = "") {
   if (window.CPS_ICONFONT_INIT) return;
+
   const ICONFIGT_SRC = src || "//at.alicdn.com/t/c/font_3959151_f86s1etjfv.js";
   const scriptElem = document.createElement("script");
   scriptElem.src = ICONFIGT_SRC;
