0f6bd851b7d27fa163b28a056ae51dc9949acaf2
cpasion-office-win10 <373704015@qq.com>
2023-04-12 19:52:26 +0800

    ✨新增(custom): 添加动态数据

--- scripts/project.ts
+++ scripts/project.ts
@@ -1,5 +1,18 @@
-import type { IprojectData } from "../src/dataTypes.d";
-import path from "path";
-const PROJECT_FILE_PATH = path.resolve("");
+/*
+ * @Author: cpasion-office-win10 373704015@qq.com
+ * @Date: 2023-04-03 16:34:21
+ * @LastEditors: cpasion-office-win10 373704015@qq.com
+ * @LastEditTime: 2023-04-12 18:54:45
+ * @FilePath: \cps-blog\scripts\project.ts
+ * @Description: 创建项目页的动态数据
+ */
 
-export const projectData: IprojectData[] = [];
+import path from "path";
+import {createNavItemByDir} from './utils'
+import { Tags } from "../src/data/project";
+import type { TagType, Project, Tag } from "../src/data/project";
+// import type { IprojectData } from "../src/dataTypes.d";
+
+const PATH_OPENSOURCE = path.resolve('./docs/【05】项目经历/完整项目')
+
+console.log({PATH_OPENSOURCE})
--- src/pages/project_my/_components/projectCards/cards.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Cards() {
-  return <div> 123123 </div>;
-}
